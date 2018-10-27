f0f7609e899249a7c4829789ad5eed7e04b13d08
Piotr Kudrel <piotr.kudrel@deneblab.com>
2018-10-27 22:31:26 +0200

    Pipeline

--- src/build/.build.ps1
+++ src/build/.build.ps1
@@ -84,7 +84,7 @@ task Update-BuildServer -If (($env:AGENT_NAME).Length -ne 0) {
 		Write-Host "Build server version: $tvc" 
 		Write-Host "Script version: $s" 
 			
-		Write-Host "##vso[release.updatereleasename]$s"
+		Write-Host "##vso[build.updatebuildnumber]$s"
 		#Write-Host "##vso[build.updatebuildnumber]my-new-build-number"
 }
 
